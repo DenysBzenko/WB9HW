a80766f0b513edef3c66b2cb7ac300e30a3a43aa
Denys <119534908+DenysBzenko@users.noreply.github.com>
2023-10-14 10:14:00 +0300

    Update functions.test.js

--- program/test/functions.test.js
+++ program/test/functions.test.js
@@ -3,9 +3,9 @@ const expect = chai.expect;
 const { getFactorial, multiply } = require('./functions'); // Імпортуємо функції з functions.js
 
 describe('getFactorial Function', function() {
-    // ... ваші тести для getFactorial ...
+    
 });
 
 describe('multiply Function (Currying)', function() {
-    // ... ваші тести для multiply ...
+
 });
